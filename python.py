6dc2685cc5eed8e65606af1283e0e93493a8b6f3
Sultan Ansari <ansarisultan443@gmail.com>
2026-08-21 15:05:04 +0530

    Delete python.py

--- python.py
+++
@@ -1,8 +0,0 @@
-name="sultan"
-My_country = "India"
-Age = 29
-print("My name is", name, "I am", Age, "years old")
-print("His name is " + name)
-print(f"I am from {My_country} and going to turn {Age} old")
-age = input("Enter your birth year")
-print("your birth year is", age)
